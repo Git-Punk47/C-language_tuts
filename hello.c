ce4fe0159bc68509287c7617b8228032624e5a9d
pankj <pankj@gmail.com>
2023-08-25 23:04:01 +0530

    1st chapter is compeleted

--- hello.c
+++ hello.c
@@ -1,6 +1,29 @@
-#include<stdio.h>
-int main(){
-    printf("heelo world");
+// #include <stdio.h>
+// int main()
+// {
+//     /* The line `printf("heelo world");` is printing the string "heelo world" to the console. */
+//     // printf("heelo world");
+//     // int x = 3;
+//     // float y = 3.14;
+    // char z = '*';
+//     // double w = 23.3232;
 
+//     // printf("space occupied by x = %d \n",sizeof(x));
+//     // printf("space occupied by y = %f \n",sizeof(y));
+    // printf("space occupied by z = %c","and size of char", sizeof(z));
+//     // printf("space occupied by w = %lf \n",sizeof(w));
+//     // printf("%d is at addressed at %u \n",&x)
+//     // printf("%f is at addressed at %u \n")
+//     // printf("%lf is at addressed at %u \n")
+    // return 0;
+// }
+
+#include<stdio.h>
+
+int main(){
+    int num = 34;
+    char star = '*';
+    int age  = 33;
+    float pi =3.14;
     return 0;
 }
